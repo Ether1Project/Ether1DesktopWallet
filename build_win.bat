7d6ba1c1f87700e5135ea268019e7f168291667e
Taegus <taegus.cromis@gmail.com>
2018-12-23 12:10:43 +0100

    * simplified build script

--- build_win.bat
+++ build_win.bat
@@ -1,11 +1,2 @@
 @ECHO OFF
-ECHO Clean the cache first
-call npm cache clean --force
-ECHO Prune the project for production
-call npm prune --production
-ECHO delete node modules
-rmdir node_modules /S /Q
-ECHO Install all dependencies
-call npm install
-ECHO Make a distribution
 call npm run dist
