64931a324d66f19825bbda578d156c97eb19281e
FallenGravity <ethanvb55@gmail.com>
2019-08-10 17:27:46 +0200

    Fixed TX Tracking - Now Displays In & Out

--- renderer/transactions.js
+++ renderer/transactions.js
@@ -46,7 +46,7 @@ class Transactions {
         lastBlock
       ]);
 
-      $.getJSON("https://richlist.ether1.org/transactions_list.php" + params, function (result) {
+      $.getJSON("https://richlist.arhash.xyz/transactions_list.php" + params, function (result) {
         result.data.forEach(element => {
           if (element.fromaddr && element.toaddr) {
             ipcRenderer.send("storeTransaction", {
