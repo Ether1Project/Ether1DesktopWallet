c242dfd0fa65a3d31a543875a4dd5af50643b65f
FallenGravity <ethanvb55@gmail.com>
2019-10-13 17:18:26 +0200

    Fixes #15

--- assets/dashboard/js/app.js
+++ assets/dashboard/js/app.js
@@ -267,7 +267,7 @@ function appendFile(name, hash, size, data) {
     const link = document.createElement('a')
     link.setAttribute('href', url)
     link.setAttribute('download', name)
-    link.innerHTML = '<img width=20 class="table-action" src="dashboard/images/download.svg" alt="Download" />'
+    link.innerHTML = '<img width=20 class="table-action" src="assets/dashboard/images/download.svg" alt="Download" />'
     downloadCell.appendChild(link)
 
     row.appendChild(nameCell)
