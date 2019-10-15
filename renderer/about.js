49acb68afa51b9e3f27ca45e002582e6450f30d6
FallenGravity <ethanvb55@gmail.com>
2019-10-15 16:39:28 +0200

    Drop mac breaking scripts.

--- renderer/about.js
+++ renderer/about.js
@@ -8,13 +8,6 @@ class About {
   renderAbout() {
     EthoMainGUI.renderTemplate("about.html", {});
     $(document).trigger("render_about");
-    let shell = require('electron').shell
-    document.addEventListener('click', function(event) {
-      if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
-        event.preventDefault()
-        shell.openExternal(event.target.href)
-      }
-    })
   }
 }
 
