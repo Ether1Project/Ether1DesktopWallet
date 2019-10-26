c9a95f1dd8299d01b8547ccd85f83d166b7e2bc8
FallenGravity <ethanvb55@gmail.com>
2019-10-26 21:44:26 +0200

    Disable Dev tools on launch

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ locker.lock().then(function() {
     EthoGeth.startGeth();
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    //mainWindow.webContents.openDevTools()
 
     // Emitted when the window is closed.
     mainWindow.on("closed", function() {
